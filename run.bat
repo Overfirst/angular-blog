d96b626c0284cc8eec32753155febaeec2828dd8
Overfirst <40057770+Overfirst@users.noreply.github.com>
2021-05-24 16:41:24 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-echo "# angular-blog" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Overfirst/angular-blog.git
-git push -u origin main
